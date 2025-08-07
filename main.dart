844e5611352ac8fccf03447a7b36f5ad09b640d4
aliabbasibu123-pixel <ali.abbasibu123@gmail.com>
2025-08-07 12:41:59 +0500

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,16 +1,11 @@
 
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:image_picker/image_picker.dart';
-import 'dart:io';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: Text('FF Tournament App')),
+      body: Center(child: Text('Welcome to FF Tournament App')),
+    ),
+  ));
 }
-
-// [rest of the code was added in previous messages, truncated here for brevity]
